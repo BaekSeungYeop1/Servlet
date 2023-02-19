d63b28d85463525b7ae74ebb500f36ff2cf954e1
DESKTOP-NT05U6L\Yeop <tmdduq5498@gmail.com>
2023-02-19 11:59:39 +0900

    1. 핸드러 매핑과 핸드러 어댑터

---
+++ src/main/java/hello/servlet/mvc/controller/Controller.java
@@ -0,0 +1,12 @@
+package hello.servlet.mvc.controller;
+
+
+
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Controller {
+    ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception;
+}
