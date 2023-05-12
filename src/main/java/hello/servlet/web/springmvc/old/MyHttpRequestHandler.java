861e7057ee475ec7b8c4cb075077487a1f2d3d2e
DESKTOP-NT05U6L\Yeop <tmdduq5498@gmail.com>
2023-05-12 21:54:50 +0900

    SpringMvc 전체적인 구조

--- src/main/java/hello/servlet/web/springmvc/old/MyHttpRequestHandler.java
+++ src/main/java/hello/servlet/web/springmvc/old/MyHttpRequestHandler.java
@@ -1,4 +1,4 @@
-package hello.servlet.springmvc.old;
+package hello.servlet.web.springmvc.old;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.HttpRequestHandler;
--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -1,4 +1,4 @@
-package hello.servlet.springmvc.old;
+package hello.servlet.web.springmvc.old;
 
 
 import org.springframework.stereotype.Component;
@@ -8,8 +8,11 @@ import org.springframework.web.servlet.mvc.Controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 @Component("/springmvc/old-controller")
 public class OldController implements Controller {
+
+
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
